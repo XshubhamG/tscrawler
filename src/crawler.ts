ab45f92d2ba50ab7826dd00bac7b5ddcb7f5dc1d
Shubham Giri <shubhammgiri3@gmail.com>
2026-03-12 17:38:49 +0530

    implement crawlPage function: recusrively crawler from a website, each of its webpage

--- src/crawler.ts
+++ src/crawler.ts
@@ -2,13 +2,13 @@ import { URL } from "node:url";
 import { JSDOM } from "jsdom";
 import { ExtractedPageData } from "./types";
 
-export function normalizeUrl(url: string): string {
+export function normalizeUrl(url: string) {
   let urlObj = new URL(url);
   let hostname = urlObj.hostname;
 
   let pathname = urlObj.pathname;
   if (pathname.endsWith("/")) {
-    pathname = pathname.slice(0, pathname.length - 1);
+    pathname = pathname.slice(0, -1);
   }
 
   return hostname + pathname;
@@ -16,54 +16,66 @@ export function normalizeUrl(url: string): string {
 
 export function getHeadingFromHTML(html: string): string {
   const dom = new JSDOM(html);
-  const heading1 = dom.window.document.querySelector("h1")?.textContent;
-  const heading2 = dom.window.document.querySelector("h2")?.textContent;
-  if (heading1) {
-    return heading1;
-  } else if (heading2) {
-    return heading2;
+  const h1 = dom.window.document.querySelector("h1")?.textContent.trim();
+  const h2 = dom.window.document.querySelector("h2")?.textContent.trim();
+  if (h1) {
+    return h1;
+  } else if (h2) {
+    return h2;
   } else {
     return "";
   }
 }
 
 export function getFirstParagraphFromHTML(html: string): string {
-  const dom = new JSDOM(html);
-  const main = dom.window.document.querySelector("main");
-  if (main) {
-    let firstParagraph = main.querySelector("p")?.textContent;
-    return firstParagraph!;
-  } else {
-    let firstParagraph = dom.window.document.querySelector("p")?.textContent;
-    return firstParagraph!;
+  try {
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+    const main = doc.querySelector("main");
+    const p = main?.querySelector("p") ?? doc.querySelector("p");
+    return (p?.textContent ?? "").trim();
+  } catch (err) {
+    return "";
   }
-  return "";
 }
 
 export function getURLsFromHTML(html: string, baseURL: string): string[] {
-  const dom = new JSDOM(html);
-  const anchorArr = [...dom.window.document.querySelectorAll("a")];
-  const urlArr = anchorArr
-    .map((a) => a.getAttribute("href"))
-    .map((url) => {
-      const u = new URL(url!, baseURL);
-      return u.href;
-    });
+  let urls: string[] = [];
+  try {
+    const dom = new JSDOM(html);
+    const anchorArr = [...dom.window.document.querySelectorAll("a")];
+    const urlArr = anchorArr
+      .map((a) => a.getAttribute("href"))
+      .map((url) => {
+        const u = new URL(url!, baseURL);
+        return u.href;
+      });
+    urls = [...new Set(urlArr)];
+  } catch (err) {
+    console.error("failed to parse HTML:", err);
+  }
 
-  return [...new Set(urlArr)];
+  return urls;
 }
 
 export function getImagesFromHTML(html: string, baseURL: string): string[] {
-  const dom = new JSDOM(html);
-  const imgTags = [...dom.window.document.querySelectorAll("img")];
-  const srcArr = imgTags
-    .map((h) => h.getAttribute("src"))
-    .map((url) => {
-      const u = new URL(url!, baseURL);
-      return u.href;
-    });
+  let images: string[] = [];
+  try {
+    const dom = new JSDOM(html);
+    const imgTags = [...dom.window.document.querySelectorAll("img")];
+    const srcArr = imgTags
+      .map((h) => h.getAttribute("src"))
+      .map((url) => {
+        const u = new URL(url!, baseURL);
+        return u.href;
+      });
 
-  return [...new Set(srcArr)];
+    images = [...new Set(srcArr)];
+  } catch (err) {
+    console.error("failed to parse HTML:", err);
+  }
+
+  return images;
 }
 
 export function extractPageData(
@@ -77,4 +89,65 @@ export function extractPageData(
     outgoingLinks: getURLsFromHTML(html, pageURL),
     imageUrls: getImagesFromHTML(html, pageURL),
   };
+}
+
+export async function getHTML(url: string) {
+  console.log(url);
+  let res;
+  try {
+    res = await fetch(url, {
+      headers: { "User-agent": "TsCrawler/1.0" },
+    });
+  } catch (err) {
+    throw new Error(`Got Network error: ${(err as Error).message}`);
+  }
+
+  if (!res.ok) {
+    console.log(`Got HTTP error: ${res.status} ${res.statusText}`);
+    return "";
+  }
+  const contentType = res.headers.get("content-type");
+  if (!contentType || !contentType?.includes("text/html")) {
+    console.log(`Got non-HTML response: ${contentType}`);
+    return "";
+  }
+
+  return res.text();
+}
+
+export async function crawlPage(
+  baseUrl: string,
+  currentUrl: string = baseUrl,
+  pages: Record<string, number> = {},
+) {
+  let current = new URL(currentUrl);
+  const base = new URL(baseUrl);
+
+  if (base.origin !== current.origin) return pages;
+
+  const normalizedCurrent = normalizeUrl(current.href);
+  if (Object.hasOwn(pages, normalizedCurrent)) {
+    pages[normalizedCurrent]++;
+    return pages;
+  }
+
+  pages[normalizedCurrent] = 1;
+
+  console.log(`Current URL: ${currentUrl}`);
+
+  let html = "";
+  try {
+    html = await getHTML(currentUrl);
+    console.log(html);
+  } catch (e) {
+    console.log(`${(e as Error).message}`);
+    return pages;
+  }
+
+  const urlsOnCurrentPage = getURLsFromHTML(html, baseUrl);
+
+  for (let i = 0; i < urlsOnCurrentPage.length; i++) {
+    pages = await crawlPage(baseUrl, urlsOnCurrentPage[i], pages);
+  }
+  return pages;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { argv } from "node:process";
+import { crawlPage } from "./crawler";
 
-function main() {
+async function main() {
   if (argv.length === 3) {
-    getHTML(argv[2]);
+    const pages = await crawlPage(argv[2]);
+    console.log(pages);
   } else {
     console.error("Unknown number of aruguments");
     process.exit(1);
@@ -10,18 +12,3 @@ function main() {
 }
 
 main();
-
-async function getHTML(url: string) {
-  try {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error(`HTTP error: ${res.status}`);
-    }
-
-    const html = await res.text();
-    console.log(html);
-  } catch (e) {
-    console.error(`Error: `, e);
-  }
-}
